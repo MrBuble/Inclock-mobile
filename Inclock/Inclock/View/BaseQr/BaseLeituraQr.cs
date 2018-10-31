ada4fea37b7d1159455f33a1e1b25648d236644c
MrBuble <oliveiramelo1996@gmail.com>
2018-10-31 07:53:42 -0300

    classe base para Leitor QR

---
+++ Inclock/Inclock/View/BaseQr/BaseLeituraQr.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Inclock.View.BaseQr
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    partial class BaseLeituraQr : ContentPage
+    {
+        public ViewModels.GetQrViewModel ViewModel = new Inclock.ViewModels.GetQrViewModel();
+        public BaseLeituraQr()
+        {
+            
+            ZXReader.BindingContext = new ViewModels.GetQrViewModel();
+            ZXReader.Options.PossibleFormats.Add(ZXing.BarcodeFormat.QR_CODE);
+        }
+    }
+}
---
+++ Inclock/Inclock/View/BaseQr/BaseLeituraQrComponents.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Inclock.View.BaseQr
+{
+    partial class BaseLeituraQr
+    {
+
+        public StackLayout StlLeitor { get; set; }
+        public Grid GrdLeitor { get; set; }
+        public ZXingScannerView ZXReader { get; set; }
+        public ZXingDefaultOverlay ZXOverlay { get; set; }
+        private void InitializeComponent()
+        {
+            ZXReader = new ZXingScannerView
+            {
+                AutomationId = "ZXingScannerView",
+                InputTransparent = true,
+                IsAnalyzing = ViewModel.IsAnalyzing,
+                IsScanning = ,
+                IsTorchOn = true,
+                Result = "{Binding Result, Mode=TwoWay}",
+                ScanResultCommand = "QRScanResultCommand"
+
+            };ZXReader.Bounds.i
+        
+            GrdLeitor = new Grid()
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            StlLeitor = new StackLayout()
+            {
+                BackgroundColor = Color.Transparent,
+                Children = { GrdLeitor }
+            };
+        }
+    }
+}
